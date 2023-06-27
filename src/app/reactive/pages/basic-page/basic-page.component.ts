e465f55378ec37a3aea99e5b65dfc0df08503fed
antoniopd <antoniopdspain@gmail.com>
2023-06-27 17:28:15 +0200

    Formularios reactivos

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 1600,
+  inStorage: 5,
+}
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl( '' ),
+  //   price: new FormControl( 0 ),
+  //   isStorage: new FormControl( 0 ),
+  // });
+
+  public myForm: FormGroup = this.fb.group({
+    name: [ '', [ Validators.required, Validators.minLength(3) ] ],
+    price: [ 0, [ Validators.required, Validators.min(0) ] ],
+    inStorage: [ 0, [ Validators.required, Validators.min(0) ] ],
+  });
+
+
+  constructor( private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.myForm.reset( rtx5090 );
+  }
+
+  onSave(): void {
+
+    if ( this.myForm.invalid ) return;
+
+    console.log( this.myForm.value );
+
+    this.myForm.reset({ price: 0, inStorage: 0 });
+  }
 }
--- src/app/reactive/reactive-routing.module.ts
+++ src/app/reactive/reactive-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
-import { DinamicPageComponent } from './pages/dinamic-page/dinamic-page.component';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
 
 const routes: Routes = [
@@ -9,7 +9,7 @@ const routes: Routes = [
     path: '',
     children: [
      { path: 'basic', component: BasicPageComponent },
-     { path: 'dinamic', component: DinamicPageComponent },
+     { path: 'dynamic', component: DynamicPageComponent },
      { path: 'switches', component: SwitchesPageComponent },
      { path: '**', redirectTo: 'basic' },
     ]
